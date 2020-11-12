7e40d9cac833e072bcfb93f8babd10b9758ce6dc
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-11-12 15:54:30 +0100

    Fix: new logic for switch

--- donation-2.js
+++ donation-2.js
@@ -264,6 +264,230 @@ parcelRequire = (function(e, r, t, n) {
       },
       { "./elementsSetup": "FbHg" },
     ],
+    Xmoj: [
+      function (require, module, exports) {
+        "use strict";
+        var e = n(require("./stripe/elements/elementsSetup")),
+          t = n(require("./stripe/createIdealSource")),
+          a = n(require("./helpers/initDOMElements"));
+        function n(e) {
+          return e && e.__esModule ? e : { default: e };
+        }
+        const i = (0, e.default)(),
+          s = i.stripe,
+          r = i.cardElement,
+          o = i.idealBank,
+          d = (0, a.default)();
+        let l = "one-time",
+          c = "plan_FUIxzfuPl4eLAs",
+          m = 1000,
+          u = r;
+        async function h(e) {
+          try {
+            if ("card" === u) {
+              const t = await axios.get(
+                  "https://www.saveachildsheartnederland.nl/setup_intent"
+                ),
+                a = await s.handleCardSetup(t.data.client_secret, r, {
+                  payment_method_data: {
+                    billing_details: { name: e.fullName, email: e.email },
+                  },
+                });
+              if (a.setupIntent) {
+                const t = await axios.post(
+                  "https://www.saveachildsheartnederland.nl/create_recurring",
+                  {
+                    paymentType: "card",
+                    paymentMethodId: a.setupIntent.payment_method,
+                    subscriptionPlanId: c,
+                    email: e.email,
+                    name: e.fullName,
+                    metadata: { newsletterSignup: e.newsletterSignup },
+                  }
+                );
+                  console.log("this is 289t")
+                  console.log(t)
+                if ("incomplete" === t.data.status) {
+                  const e = await s.handleCardPayment(
+                    t.data.latest_invoice.payment_intent.client_secret
+                  );
+                  e.error ? (d.formErrors.textContent = e.error.message) : v();
+                } else "active" === t.data.status && v();
+              } else
+                a.error &&
+                  ((d.formErrors.textContent = a.error.message),
+                  console.error(a.error));
+            } else {
+              const a = m,
+                n = {
+                  email: e.email,
+                  newsletterSignup: e.newsletterSignup,
+                  description: PAYMENT_DESCRIPTION,
+                  subscriptionPlanId: c,
+                  recurring: !0,
+                  REDIRECT_URL: REDIRECT_URL,
+                },
+                i = await (0, t.default)(e.fullName, a, n, o),
+                s = i.source,
+                r = i.error;
+              s
+              console.log("This is n315")
+              console.log(n)
+                ? (document.location.href = s.redirect.url)
+                : r && (d.formErrors.textContent = r.message);
+            }
+          } catch (a) {
+            d.formErrors.textContent = a.message;
+          }
+        }
+        function p(e) {
+          const t = e.data,
+            a = t.error,
+            n = t.requires_action,
+            i = t.payment_intent_client_secret;
+          a
+            ? ((d.formErrors.textContent = a.message),
+              d.submitBtn.classList.add("button-failed", "button-disabled"),
+              (d.submitBtn.disabled = !0),
+              (d.submitBtn.value = "An error occured."))
+            : n
+            ? s.handleCardAction(i).then((e) => {
+                e.error
+                  ? ((d.formErrors.textContent = e.error.message),
+                    d.submitBtn.classList.add(
+                      "button-failed",
+                      "button-disabled"
+                    ),
+                    (d.submitBtn.disabled = !0),
+                    (d.submitBtn.value = "An error occured."),
+                    console.warn(e.error))
+                  : axios
+                      .post("https://www.saveachildsheartnederland.nl/confirm_payment", {
+                        paymentIntentId: e.paymentIntent.id,
+                      })
+                      .then(p)
+                      .catch((e) => {
+                        d.submitBtn.classList.add(
+                          "button-failed",
+                          "button-disabled"
+                        ),
+                          (d.submitBtn.disabled = !0),
+                          (d.submitBtn.value = "Your card was declined.");
+                      });
+              })
+            : v();
+        }
+        function v() {
+          d.submitBtn.classList.add("button-success", "button-disabled"),
+            (d.submitBtn.disabled = !0),
+            (d.submitBtn.value = "Donatie verwerkt!"),
+            setTimeout(() => window.location.replace(REDIRECT_URL), 1500);
+        }
+        d.form.addEventListener("submit", async (e) => {
+          e.preventDefault(), (d.submitBtn.value = "Een moment geduld...");
+          const a = d.fullNameField.value,
+            n = d.emailField.value,
+            i = d.newsletterCheckbox.checked,
+            c = { email: n, fullName: a, newsletterSignup: i };
+          if ("monthly" === l) h(c);
+          else {
+            const e = Math.round(100 * parseInt(d.amountField.value));
+            if ("card" === u) {
+              const t = await s.createPaymentMethod("card", r, {
+                  billing_details: { name: c.fullName, email: c.email },
+                }),
+                a = t.paymentMethod,
+                o = t.error;
+              if (o) d.formErrors.textContent = o.message;
+              else {
+                (d.formErrors.textContent = ""),
+                  p(
+                    await axios.post(
+                      "https://www.saveachildsheartnederland.nl/confirm_payment",
+                      {
+                        paymentMethodId: a.id,
+                        amount: e,
+                        email: n,
+                        newsletterSignup: i,
+                        description: PAYMENT_DESCRIPTION,
+                      }
+                    )
+                  );
+                    console.log("This is amount: e")
+                    console.log(e)
+              }
+            } else {
+              const s = {
+                  email: n,
+                  newsletterSignup: i,
+                  description: PAYMENT_DESCRIPTION,
+                  REDIRECT_URL: REDIRECT_URL,
+                },
+                r = await (0, t.default)(a, e, s, o),
+                l = r.source,
+                c = r.error;
+              l
+                ? (document.location.href = l.redirect.url)
+                : c && (d.formErrors.textContent = c.message);
+            }
+          }
+        }),
+          d.oneTimeSwitch.addEventListener("click", () => {
+            d.oneTimeSwitch.classList.remove("not-active"),
+              d.monthlySwitch.classList.add("not-active"),
+              d.amountContainer.classList.remove("hidden"),
+              d.amountField.setAttribute("required", !0),
+              d.donationPlansWrapper.classList.add("hidden"),
+              d.mandatePolicy.classList.add("hidden"),
+              (l = "one-time");
+          }),
+          d.monthlySwitch.addEventListener("click", () => {
+            d.monthlySwitch.classList.remove("not-active"),
+              d.oneTimeSwitch.classList.add("not-active"),
+              d.amountContainer.classList.add("hidden"),
+              d.amountField.removeAttribute("required"),
+              d.donationPlansWrapper.classList.remove("hidden"),
+              d.mandatePolicy.classList.remove("hidden"),
+              (l = "monthly");
+          }),
+          d.donationPlansButtons.forEach((e) => {
+            e.addEventListener("click", (e) => {
+              const t = e.target;
+              t.classList.remove("not-active"),
+                d.donationPlansButtons.forEach(
+                  (e) => e !== t && e.classList.add("not-active")
+                ),
+                (c = t.dataset.planId),
+                (m = t.dataset.amount);
+            });
+          }),
+          d.moreAmountsBtn.addEventListener("click", () => {
+            d.hiddenPlansButtons.classList.remove("hidden"),
+              d.moreAmountsBtn.classList.add("hidden");
+          }),
+          d.cardSwitch.addEventListener("click", (e) => {
+            e.preventDefault(),
+              d.cardSwitch.classList.remove("inactive"),
+              d.iDealSwitch.classList.add("inactive"),
+              d.cardSection.classList.remove("hidden"),
+              d.iDealSection.classList.add("hidden"),
+              (u = "card");
+          }),
+          d.iDealSwitch.addEventListener("click", (e) => {
+            e.preventDefault(),
+              d.iDealSwitch.classList.remove("inactive"),
+              d.cardSwitch.classList.add("inactive"),
+              d.iDealSection.classList.remove("hidden"),
+              d.cardSection.classList.add("hidden"),
+              (u = "ideal");
+          });
+      },
+      {
+        "./stripe/elements/elementsSetup": "XxEM",
+        "./stripe/createIdealSource": "GhFC",
+        "./helpers/initDOMElements": "Xa8A",
+      },
+    ],
   },
   {},
   ["Focm"],
