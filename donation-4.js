8336ce0c2da70e3861ba0ad5be080e734a422d97
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-02-08 14:24:30 +0100

    Fix 7

--- donation-4.js
+++ donation-4.js
@@ -397,12 +397,11 @@ parcelRequire = (function(e, r, t, n) {
               }
             } else {
               const s = {
-                  metadata {
+                  metadata: {
                     email: n,
                     newsletterSignup: i,
                     description: PAYMENT_DESCRIPTION,
                     REDIRECT_URL: REDIRECT_URL,
-
                   },
                 },
                 r = await (0, t.default)(a, e, s, o),
